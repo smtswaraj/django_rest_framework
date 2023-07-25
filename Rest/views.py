58a4dc2b45f66039abccb206a25b041a50eac024
Swaraj <swarajnayakdipu227@gmail.com>
2023-07-26 03:01:42 +0530

    commit at 1:08:05

--- Rest/views.py
+++ Rest/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import TodoSerializer,TimingTodoSerializer
-from .models import Todo
+from .models import Todo,TimingTodo
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -144,6 +144,18 @@ class TodoView(APIView):
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    @action(detail=False, methods=['get'])
+    def get_timing_todo(self, request):
+        objs = TimingTodo.objects.all()
+        serializer = TimingTodoSerializer(objs, many = True)
+        return Response({
+                        'status':True,
+                        'message':'Timing todo fetched',
+                        'data': serializer.data
+                    })
+
+
 
     @action(detail=False, methods=['post'])
     def add_date_to_todo(self , request):
